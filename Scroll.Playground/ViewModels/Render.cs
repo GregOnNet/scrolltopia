17a8e5a1502d981dd8a523f4712fc32b78e842b8
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-08-29 18:37:06 +0200

    Adds Wrapper to provide Model and RenderMode to partial templates

---
+++ Scroll.Playground/ViewModels/Render.cs
@@ -0,0 +1,25 @@
+﻿using Scroll.Playground.Models;
+
+namespace Scroll.Playground.ViewModels
+{
+    public class Render<T>
+    {
+        public static Render<T> Model(T model) {
+
+            return new Render<T>
+            {
+                Content = model
+            };
+            
+        }
+
+        public T Content { get; set; }
+        public ERender RenderMode { get; private set; }
+
+        public Render<T> As(ERender renderMode)
+        {
+            RenderMode = renderMode;
+            return this;
+        }
+    }
+}
