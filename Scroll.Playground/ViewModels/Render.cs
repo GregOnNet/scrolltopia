cb474cf210e9344dd517d993f3db708837d906b0
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-08-29 20:36:23 +0200

    Documents Render<>

--- Scroll.Playground/ViewModels/Render.cs
+++ Scroll.Playground/ViewModels/Render.cs
@@ -2,15 +2,21 @@
 
 namespace Scroll.Playground.ViewModels
 {
+    /// <summary>
+    /// Wraps any Model and provides a type (<see cref="ERender"/>)
+    /// to decide how the model should be rendered.
+    /// E.g. as content or menu
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Render<T>
     {
-        public static Render<T> Model(T model) {
-
+        public static Render<T> Model(T model)
+        {
             return new Render<T>
             {
                 Content = model
             };
-            
+
         }
 
         public T Content { get; set; }
