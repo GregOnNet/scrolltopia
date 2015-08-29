dfc060aa1a235c78a49993a383fa8b75ac9a328e
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-08-29 20:25:02 +0200

    Introduces Render<> to SimpleBarDemos

--- Scroll.Playground/Controllers/SimpleBarController.cs
+++ Scroll.Playground/Controllers/SimpleBarController.cs
@@ -1,12 +1,20 @@
 ﻿using System.Web.Mvc;
+using Scroll.Playground.DemoRepositories;
 
 namespace Scroll.Playground.Controllers
 {
     public class SimplebarController : Controller
     {
+        private readonly SimplebarDemos _demos;
+
+        public SimplebarController()
+        {
+            _demos = new SimplebarDemos();
+        }
+
         public ViewResult Index()
         {
-            return View();
+            return View(_demos.GetAll());
         }
 
         public ViewResult MinimalSetup()
---
+++ Scroll.Playground/DemoRepositories/SimplebarDemos.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using Scroll.Playground.Models;
+
+namespace Scroll.Playground.DemoRepositories
+{
+    public class SimplebarDemos
+    {
+        public ControllerDemoCollection GetAll()
+        {
+            return new ControllerDemoCollection
+            {
+                ControllerName = "Simplebar",
+                Demos = new List<DemoSection>
+                {
+                    new DemoSection
+                    {
+                        Title = "Introduction",
+                        Demos = new List<Demo>
+                        {
+                            new Demo {Title = "Start", ActionName = "Index"}
+                        }
+                    },
+                    new DemoSection
+                    {
+                        Title = "Basic",
+                        Demos = new List<Demo>
+                        {
+                            new Demo {Title = "Minimal", ActionName = "MinimalSetup"},
+                            new Demo {Title = "Scrollbar", ActionName = "Scrollbar"},
+                        }
+                    },
+                    new DemoSection
+                    {
+                        Title = "Interaction",
+                        Demos = new List<Demo>
+                        {
+                        }
+                    },
+                    new DemoSection
+                    {
+                        Title = "Dynamic DOM",
+                        Demos = new List<Demo>
+                        {
+                        }
+                    },
+                    new DemoSection
+                    {
+                        Title = "Events",
+                        Demos = new List<Demo>
+                        {
+                        }
+                    }
+                }
+            };
+        }
+    }
+ }
